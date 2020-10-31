7661ae95af63b6fdb4a341b48102258bf18e3087
leeo2020 <3145875098@qq.com>
2020-10-31 09:52:53 +0800

    master

---
+++ src/cmake/Modules/FindProtobuf.cmake
@@ -0,0 +1,4 @@
+set(Protobuf_FOUND "True")
+set(Protobuf_VERSION "8.2.0")
+set(Protobuf_INCLUDE_DIRS "E:/Cplus/msys64/mingw64/include")
+set(Protobuf_LIBARIES "E:/Cplus/msys64/mingw64/lib/make")
---
+++ src/page001.cpp
@@ -0,0 +1,56 @@
+﻿#include <vtkSphereSource.h>
+#include <vtkCylinderSource.h>
+#include <vtkPolyData.h>
+#include <vtkSmartPointer.h>
+#include <vtkPolyDataMapper.h>
+#include <vtkActor.h>
+#include <vtkRenderWindow.h>
+#include <vtkRenderer.h>
+#include <vtkRenderWindowInteractor.h>
+#include <vtkProperty.h>
+#include <vtkAutoInit.h>  
+#include <vtkInteractorStyleTrackballCamera.h>
+
+VTK_MODULE_INIT(vtkRenderingOpenGL2);
+VTK_MODULE_INIT(vtkInteractionStyle);
+
+int main()
+{
+	vtkSmartPointer<vtkCylinderSource>cylinder =
+		vtkSmartPointer<vtkCylinderSource>::New();
+	cylinder->SetHeight(3.0);
+	cylinder->SetRadius(1.0);
+	cylinder->SetResolution(10);
+
+	vtkSmartPointer<vtkPolyDataMapper>cylinderMapper =
+		vtkSmartPointer<vtkPolyDataMapper>::New();
+	cylinderMapper->SetInputConnection(cylinder->GetOutputPort());
+
+	vtkSmartPointer<vtkActor>cylinderActor =
+		vtkSmartPointer<vtkActor>::New();
+	cylinderActor->SetMapper(cylinderMapper);
+
+	vtkSmartPointer<vtkRenderer>renderer =
+		vtkSmartPointer<vtkRenderer>::New();
+	renderer->AddActor(cylinderActor);
+	renderer->SetBackground(0.1, 0.2, 0.4);
+
+	vtkSmartPointer<vtkRenderWindow>renWin =
+		vtkSmartPointer<vtkRenderWindow>::New();
+	renWin->AddRenderer(renderer);
+	renWin->SetSize(300, 300);
+
+	vtkSmartPointer<vtkRenderWindowInteractor>iren =
+		vtkSmartPointer<vtkRenderWindowInteractor>::New();
+	iren->SetRenderWindow(renWin);
+
+	vtkSmartPointer<vtkInteractorStyleTrackballCamera>style =
+		vtkSmartPointer<vtkInteractorStyleTrackballCamera>::New();
+	iren->SetInteractorStyle(style);
+
+	iren->Initialize();
+	iren->Start();
+
+
+	return EXIT_SUCCESS;
+}
